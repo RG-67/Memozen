4c64f08b4f10333e8f3d051b8779937b79331497
RG-67 <rittikghoshkolkata@gmail.com>
2025-01-06 09:54:20 +0530

    6th Jan, 2024

---
+++ src/SampleModel/NotesData.js
@@ -0,0 +1,46 @@
+const Notes = [
+    {
+        id: 1,
+        title: "Study.....",
+        time: "10:30 AM",
+        cardTopBg: "",
+        cardBg: "",
+        iconColor: ""
+    },
+    {
+        id: 2,
+        title: "Work.....",
+        time: "12:00 PM",
+        cardTopBg: "",
+        cardBg: "",
+        iconColor: ""
+    },
+    {
+        id: 3,
+        title: "Personal.....",
+        time: "04:00 AM",
+        cardTopBg: "",
+        cardBg: "",
+        iconColor: ""
+    },
+    {
+        id: 4,
+        title: "Shopping.....",
+        time: "05:30 AM",
+        cardTopBg: "",
+        cardBg: "",
+        iconColor: ""
+    },
+    {
+        id: 5,
+        title: "Misc.....",
+        time: "09:15 PM",
+        cardTopBg: "",
+        cardBg: "",
+        iconColor: ""
+    }
+];
+
+
+
+export default Notes;
--- src/screens/TabScreens/Note.js
+++ src/screens/TabScreens/Note.js
@@ -1,21 +1,46 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Colors from "../../styles/Colors";
+import F6Icon from 'react-native-vector-icons/FontAwesome6'
+import MIcon from 'react-native-vector-icons/MaterialIcons';
 
 const Note = () => {
     return (
         <View style={styles.mainContainer}>
-            <Text>Note</Text>
+
+            <View style={styles.headerStyle}>
+                <Pressable onPress={() => { }}>
+                    <F6Icon name="arrow-left-long" size={25} />
+                </Pressable>
+                <Text style={styles.heading}>Today's Tasks</Text>
+                <View>
+                    <MIcon name="notifications" size={30} />
+                </View>
+            </View>
+
         </View>
     )
 };
 
 
 
-const styles  = StyleSheet.create({
+const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
         backgroundColor: Colors.white
-    }
+    },
+    headerStyle: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+        marginTop: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    heading: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: Colors.black,
+        alignSelf: 'center'
+    },
 });
 
 
